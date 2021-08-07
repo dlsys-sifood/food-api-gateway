b133508377e61a71e5a4a7f288dd7e555ff66c5f
salvador5120 <salvador.delaisla.mendoza@gmail.com>
2021-08-06 23:21:11 -0500

    initial api gateway

---
+++ src/main/java/com/dlsys/sifood/ags/ApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dlsys.sifood.ags;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServiceApplication.class, args);
+	}
+
+}
